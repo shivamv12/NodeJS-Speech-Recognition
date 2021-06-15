64adf9b6d71bd95e823cc173bd4f9138f30271f9
Shivam <devshivam1996@gmail.com>
2021-06-15 20:26:45 +0530

    Resolve some vulnerabilities & fixed the unmute recognition

--- public/RemoteControl.js
+++ public/RemoteControl.js
@@ -1,21 +1,19 @@
-
 class RemoteControl {
+  constructor(element) {
+    this.videoElement = element;
+  }
 
-	constructor(element) {
-		this.videoElement = element;
-	}
-
-	play() {
-		this.videoElement.play();
-	}
-	stop() {
-		this.videoElement.pause();
-		this.videoElement.currentTime = 0;
-	}
-	mute() {
-		this.videoElement.muted = true;
-	}
-	unmute() {
-		this.videoElement.muted = false;
-	}
+  play() {
+    this.videoElement.play();
+  }
+  stop() {
+    this.videoElement.pause();
+    this.videoElement.currentTime = 0;
+  }
+  mute() {
+    this.videoElement.muted = true;
+  }
+  unmute() {
+    this.videoElement.muted = false;
+  }
 }
